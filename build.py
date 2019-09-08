3f0172239086e7981eb5160dd33eb6f02ff7153f
Altair Wei <altair_wei@outlook.com>
2019-09-08 22:36:15 +0800

    only build for x86_64

--- build.py
+++ build.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(
         username="altairwei",
         upload = "https://api.bintray.com/conan/altairwei/conan",
-        remotes= "https://api.bintray.com/conan/bincrafters/public-conan, https://api.bintray.com/conan/conan-community/conan")
+        remotes= "https://api.bintray.com/conan/bincrafters/public-conan, https://api.bintray.com/conan/conan-community/conan",
+        archs = ["x86_64"])
     builder.add_common_builds(pure_c=False)
     # Add extra options to common builds
     extra_builds = []
